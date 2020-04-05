016bd0d83c19b0df217c34c5c87e319b144ca9ba
Ewing Eng <eeng275@gmail.com>
2020-04-04 22:02:50 -0400

    added initial fee

--- BasiliskTroops/BasiliskTroops.cs
+++ BasiliskTroops/BasiliskTroops.cs
@@ -41,11 +41,17 @@ namespace BasiliskTroops
         public void AddTroopDialog(CampaignGameStarter obj)
         {
 
-            obj.AddGameMenuOption("town", "info_troop_type", "Hire Basilisk Guild Troops", (MenuCallbackArgs args) => { args.optionLeaveType = GameMenuOption.LeaveType.Recruit; return true; }, this.game_menu_troop_type_on_consq, false, 5);
-            obj.AddGameMenu("town_mod_troop_type", "Basilisk Guild hiring here", (MenuCallbackArgs args) =>{ args.optionLeaveType = GameMenuOption.LeaveType.Recruit; });
-            obj.AddGameMenuOption("town_mod_troop_type", "militia_type", "Commoners", (MenuCallbackArgs args) => { args.optionLeaveType = GameMenuOption.LeaveType.Recruit; return true; }, this.conversation_miltia_on_consequence);
-            obj.AddGameMenuOption("town_mod_troop_type", "noble_type", "Nobles", (MenuCallbackArgs args) => { args.optionLeaveType = GameMenuOption.LeaveType.Recruit; return true; }, this.conversation_noble_on_consequence);
-            obj.AddGameMenuOption("town_mod_troop_type", "nevermind", "Nevermind", (MenuCallbackArgs args) => { args.optionLeaveType = GameMenuOption.LeaveType.Leave; return true; }, this.game_menu_switch_to_town_menu);
+            obj.AddGameMenuOption("town", "info_troop_type", "Hire Basilisk Guild Troops", (MenuCallbackArgs args) => { args.optionLeaveType = GameMenuOption.LeaveType.Recruit; return true; }, game_menu_troop_type_on_consq, false, 5);
+
+            Town town = Settlement.CurrentSettlement.Town;
+            int cost = (int)Math.Ceiling(town.Prosperity * 2);
+            obj.AddGameMenu("town_mod_troop_type", "The Basilisk Guild offers its mercenaries, both commoners and nobles, in every town for quite the coin. The guild leader tells you that there is an initial fee of " + cost + " denars here just for the guild to show you their mercenaries.", null);
+            obj.AddGameMenuOption("town_mod_troop_type", "pay_fee", "Pay initial cost of " + cost + " denars",
+                (MenuCallbackArgs args) => { args.optionLeaveType = GameMenuOption.LeaveType.Recruit; return true; }, game_menu_troop_type_on_consq);
+
+            obj.AddGameMenuOption("town_mod_troop_type", "militia_type", "Hire Commoners", (MenuCallbackArgs args) => { args.optionLeaveType = GameMenuOption.LeaveType.Recruit; return true; }, conversation_miltia_on_consequence);
+            obj.AddGameMenuOption("town_mod_troop_type", "noble_type", "Hire Nobles", (MenuCallbackArgs args) => { args.optionLeaveType = GameMenuOption.LeaveType.Recruit; return true; }, conversation_noble_on_consequence);
+            obj.AddGameMenuOption("town_mod_troop_type", "nevermind", "I'm done looking", (MenuCallbackArgs args) => { args.optionLeaveType = GameMenuOption.LeaveType.Leave; return true; }, game_menu_switch_to_town_menu);
         }
 
         private void game_menu_troop_type_on_consq(MenuCallbackArgs args)
@@ -62,14 +68,13 @@ namespace BasiliskTroops
         {
             MobileParty troops = new MobileParty();
             Town town = Settlement.CurrentSettlement.Town;
-            float prosp = town.Prosperity;
-            int troopAmount = (int)Math.Ceiling(prosp / 300);
+            int troopAmount = (int)Math.Ceiling(town.Prosperity / 200);
 
             foreach(string id in militiaTroopIDs)
             {
-                if(troopAmount / 2f >= 1)
+                if ((int) Math.Floor(troopAmount / 2f) >= 1)
                 {
-                    troopAmount /= 2;
+                    troopAmount = (int)Math.Floor(troopAmount / 2f);
                     troops.AddElementToMemberRoster(CharacterObject.Find(id), troopAmount);
                 } else
                 {
@@ -83,14 +88,13 @@ namespace BasiliskTroops
         {
             MobileParty troops = new MobileParty();
             Town town = Settlement.CurrentSettlement.Town;
-            float prosp = town.Prosperity;
-            int troopAmount = (int)Math.Ceiling(prosp / 300);
+            int troopAmount = (int)Math.Ceiling(town.Prosperity / 800);
 
             foreach (string id in nobleTroopIDs)
             {
-                if (troopAmount / 2f >= 1)
+                if ((int)Math.Floor(troopAmount / 2f) >= 1)
                 {
-                    troopAmount /= 2;
+                    troopAmount = (int)Math.Floor(troopAmount / 2f);
                     troops.AddElementToMemberRoster(CharacterObject.Find(id), troopAmount);
                 }
                 else
@@ -109,7 +113,7 @@ namespace BasiliskTroops
             }
             catch(NullReferenceException nope)
             {
-
+                
             }
         }
     }
---
+++ BasiliskTroops/TroopProperties.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameMenus;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.Overlay;
+using TaleWorlds.CampaignSystem.Character;
+using TaleWorlds.SaveSystem;
+using static TaleWorlds.CampaignSystem.SettlementComponent;
+
+namespace BasiliskTroops
+{
+    public class TroopProperties
+    {
+
+        [SaveableField(1)]
+        public string settlementID;
+
+        public TroopProperties(string settlementID)
+        {
+            this.settlementID = settlementID;
+        }
+
+    }
+}
